286d2ffe986f2b588a6b42f74d8a95cd17df698f
[firstname lastname] <rishabhkdayal@gmail.com>
2021-07-11 01:29:15 -0500

    first commit

---
+++ scrape.py
@@ -0,0 +1,12 @@
+#Import pprint
+
+# Import request and beautiful soup
+import requests
+from bs4 import BeautifulSoup
+
+#get the response back from the URL
+
+res = requests.get('https://news.ycombinator.com/news')
+
+soup = BeautifulSoup(res.text, 'html.parser')
+
